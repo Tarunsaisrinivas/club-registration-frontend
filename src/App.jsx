0f325e9390cdd6aac95cc440f65b20c562b345a6
Tarun Sai Srinivas <tarunsaisrinivas7@gmail.com>
2024-08-28 15:43:06 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
 // frontend/src/App.js
 
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import StudentForm from './components/StudentForm';
-import StudentList from './components/StudentList';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import StudentForm from "./components/StudentForm";
+import StudentList from "./components/StudentList";
 
 function App() {
-    return (
-        <Router>
-            <div className="min-h-screen bg-gray-100">
-              
-                <main className="p-8">
-                    <Routes>
-                        <Route path="/" element={<StudentForm />} />
-                        <Route path="/students" element={<StudentList />} />
-                    </Routes>
-                </main>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<StudentForm />} />
+        <Route path="/students" element={<StudentList />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;

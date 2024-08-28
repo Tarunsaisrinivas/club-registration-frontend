a6bd743d2fb0ad5e46ddfae99f242acc17c4b2f0
Tarun Sai Srinivas <tarunsaisrinivas7@gmail.com>
2024-08-28 15:50:56 +0530

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        // Other external dependencies can be listed here
+        'react-router-dom'
+      ]
+    }
+  }
 })

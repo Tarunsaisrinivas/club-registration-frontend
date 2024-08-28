b9fe957b1afdffec7c5649345e622d8b68c8a8fa
Tarun Sai Srinivas <tarunsaisrinivas7@gmail.com>
2024-08-28 15:57:13 +0530

    vite remove

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        // Other external dependencies can be listed here
-        'react-router-dom'
-      ]
-    }
-  }
+  
 })

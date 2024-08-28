f9133a1ac312ca6d601927df2c88cbe965f91c20
Tarun Sai Srinivas <tarunsaisrinivas7@gmail.com>
2024-08-28 16:01:10 +0530

    backend linked

--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -12,7 +12,7 @@ function StudentList() {
 
     const fetchStudents = async () => {
         try {
-            const response = await axios.get('http://localhost:5000/api/students');
+            const response = await axios.get('https://club-registration-backend.vercel.app/api/students');
             setStudents(response.data);
         } catch (error) {
             console.error('Error fetching students:', error);
